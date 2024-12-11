461fedeb9842924063ab0e22eea7d6cb4355c213
Mika Rottländer <mika.rottlaender@check24.de>
2024-12-11 23:46:56 +0100

    change data fetch logic

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,26 @@
+import { getAllCrons, getAllDevices } from "./actions";
 import CronContainer from "./components/cron/CronContainer";
 import DeviceContainer from "./components/devices/DeviceContainer";
 import styles from "./page.module.scss";
 
 export default async function Home() {
-  const baseUrl = process.env.BASE_API_URL;
-  const cronData = await fetch(`${baseUrl}/crons`, {
-    headers: {
-      Authorization: `Bearer ${process.env.API_KEY}`,
-    },
-  });
-  const cronJson = await cronData.json();
+  const results = await Promise.allSettled([getAllCrons(), getAllDevices()]);
+  if (results.some((result) => result.status === "rejected")) {
+    return <div>Failed to fetch data</div>;
+  }
 
-  const deviceData = await fetch(`${baseUrl}/devices`, {
-    headers: {
-      Authorization: `Bearer ${process.env.API_KEY}`,
-    },
-  });
-  const deviceJson = await deviceData.json();
+  const [cronsResult, devicesResult] = results;
+
+  const crons =
+    cronsResult.status === "fulfilled" ? cronsResult.value.crons : [];
+  const devices =
+    devicesResult.status === "fulfilled" ? devicesResult.value.devices : [];
 
   return (
     <div className={styles.page}>
       <>
-        <CronContainer crons={cronJson.crons} />
-        <DeviceContainer devices={deviceJson.devices} />
+        <CronContainer crons={crons} />
+        <DeviceContainer devices={devices} />
       </>
     </div>
   );
